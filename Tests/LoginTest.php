46f1d1b29d5a8a7e23a8f762c3206904df4b7300
iunia <iunia.bujita@softvision.ro>
2014-06-17 12:23:38 +0300

    test folder added

---
+++ Tests/LoginTest.php
@@ -0,0 +1,30 @@
+<?php
+class StackTest extends PHPUnit_Framework_TestCase
+{
+    public function testAuthenticate()
+    {
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+    
+     public function testLogout()
+    {
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+}
+?>
