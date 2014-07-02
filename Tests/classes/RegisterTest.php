73ff84a60638ef2a3637c2c4a071f28281ac4ffa
Iunia Bujita <iunia.bujita@softvision.ro>
2014-07-02 13:50:42 +0300

    Update RegisterTest.php

--- Tests/classes/RegisterTest.php
+++ Tests/classes/RegisterTest.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once 'classes/Register.php';
 require_once 'classes/User.php';
 require_once 'classes/Db.php';
@@ -9,12 +10,13 @@ class RegisterTest extends PHPUnit_Framework_TestCase {
 
     protected function setUp() {
 
-        $this->register = new Register();
+        $this->register = new Register();  //create a new Register
         $class = get_class($this->register);
         $this->assertEquals($class, "Register");
     }
 
     public function testValidateFields() {
+        //try to validate incorrect values for fields
         $resultF1 = $this->register->validateFields(null, null, null, null, null);
         $this->assertFalse($resultF1);
         $resultF2 = $this->register->validateFields(' ', ' ', ' ', ' ', ' ');
@@ -25,13 +27,16 @@ class RegisterTest extends PHPUnit_Framework_TestCase {
         $this->assertFalse($resultF4);
         $resultF5 = $this->register->validateFields('aaaaa', 'aaaa', 'aaa@aaa.com', '12345', '123456');
         $this->assertFalse($resultF5);
+        //try to validate correct values for fields
         $resultT = $this->register->validateFields('iunia', 'bujita', 'iunia.bujita@softvision.ro', '12345', '12345');
         $this->assertTrue($resultT);
     }
 
     public function testUserRegistration() {
+        //try to register with null values
         $this->assertFalse($this->register->userRegistration(null, null, null, null));
-        $this->assertTrue($this->register->userRegistration('test', 'user', 'test.user@softvision.ro', 12345));
+        //try to register with null values
+        $this->assertEquals($this->register->userRegistration('test', 'user', 'test.user@softvision.ro', 12345), 1);
     }
 
 }
