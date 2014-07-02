be50f5fc2474cd2d495ab7b74f9854f7b9309eca
Iunia Bujita <iunia.bujita@softvision.ro>
2014-07-02 13:51:16 +0300

    Update UserTest.php

--- Tests/classes/UserTest.php
+++ Tests/classes/UserTest.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once 'classes/Db.php';
 require_once 'classes/User.php';
+require_once 'classes/Db.php';
 
 class UserTest extends PHPUnit_Framework_TestCase {
 
@@ -9,39 +9,36 @@ class UserTest extends PHPUnit_Framework_TestCase {
 
     protected function setUp() {
 
-//       $this->db = $this->getMockBuilder('Db')
-//                -> setConstructorArgs(array())
-//                ->getMock(); //new mock db
-        $this->user = new User();
+        $this->user = new User();  //create a new user
         $class = get_class($this->user);
         $this->assertEquals($class, "User");
-        //var_dump($this->user);
-//        var_dump($this->db);
     }
 
     public function testAuthenticate() {
-
+        //tests for incorrect authentication credentials
         $resultF1 = $this->user->authenticate(null, null);
         $this->assertFalse($resultF1);
-        $resultF2 = $this->user->authenticate('  ', '  ');
-        $this->assertFalse($resultF2);
         $resultF3 = $this->user->authenticate('123', '123');
         $this->assertFalse($resultF3);
+        $resultF4 = $this->user->authenticate('abcdf', 'aaaa');
+        $this->assertFalse($resultF4);
+        //test for correct authentication credentials
         $resultT = $this->user->authenticate('iunia.bujita@softvision.ro', '12345');
         $this->assertNotEmpty($resultT);
     }
 
     public function testRegister() {
-        $resultF = $this->user->register(null, null, null, null);
-        $this->assertEquals($resultF, 0);
-        $resultT = $this->user->register('test', 'user', 'usr@softvision.ro', '12345');
-        $this->assertEquals($resultT, 1);
+        //try to register with null values
+        $this->assertFalse($this->user->register(null, null, null, null));
+        //try to register with correct values
+        $this->assertTrue($this->user->register('test', 'user', 'test.user@softvision.ro', 12345));
     }
 
-   public function testGetName() {
-        
-        $this->assertFalse($this->user->userRegistration("_!?", 123, 'aaaa2', null));
-        $this->assertEquals($this->user->userRegistration('test', 'user', 'test.user@softvision.ro', 12345), null);
-        $this->assertTrue(true);
+    public function testGetName() {
+        //try to get the name of an inexistent user
+        $this->assertNull($this->user->getName('dassda', 'dsasdasdasdf'));
+        //try to get the name of an existent user
+        $this->assertEquals($this->user->getName('iunia.bujita@softvision.ro', '12345'), 'iunia');
     }
+
 }
